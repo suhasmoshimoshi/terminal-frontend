9091a571f1a6a68afb4360e3e5d5dab13baab5e4
suhasmoshimoshi <suhas09.moshimoshi@gmail.com>
2024-05-30 19:14:31 +0530

    edit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addMarkdown, removeMarkdown } from '../reducers/savedMarkdownSlice';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
 
 function Editor({ onMarkdownChange }) {
   const [markdown, setMarkdown] = useState('');
@@ -22,7 +24,29 @@ function Editor({ onMarkdownChange }) {
   };
 
   const copyMarkdownExample = () => {
-    const markdownExample = `# Markdown Example\n\nThis is a **bold** text.\n\n## Lists\n\n- Item 1\n- Item 2\n- Item 3\n\n## Code Block\n\`\`\`javascript\nfunction greet(name) {\n  console.log(\`Hello, \${name}!\`);\n}\n\ngreet('World');\n\`\`\`\n\n## Links\n\n[Neokred](https://neokred.tech/)`;
+    const markdownExample = `# Markdown Example
+
+This is a **bold** text.
+
+## Lists
+
+- Item 1
+- Item 2
+- Item 3
+
+## Code Block
+
+\`\`\`javascript
+function greet(name) {
+ console.log(\`Hello, \${name}!\`);
+}
+
+greet('World');
+\`\`\`
+
+## Links
+
+[Neokred](https://neokred.tech/)`;
     navigator.clipboard.writeText(markdownExample);
     setCopied(true);
   };
@@ -30,13 +54,24 @@ function Editor({ onMarkdownChange }) {
   return (
     <div className="flex flex-col h-screen overflow-y-auto bg-black p-4">
       <div className="flex gap-5 sm:gap-2 justify-between mb-4">
-        <button className="text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700" onClick={() => setShowTab(!showTab)}>
+        <button
+          className="text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700"
+          onClick={() => setShowTab(!showTab)}
+        >
           {showTab ? 'Hide Saved Markdowns' : 'Show Saved Markdowns'}
         </button>
-        <button className={`text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700 ${copied ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={copyMarkdownExample}>
+        <button
+          className={`text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700 ${
+            copied ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+          onClick={copyMarkdownExample}
+        >
           {copied ? 'Copied!' : 'Copy Markdown Example'}
         </button>
-        <button className="text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700" onClick={handleSaveMarkdown}>
+        <button
+          className="text-green-500 font-mono px-2 py-1 mb-2 rounded bg-gray-700"
+          onClick={handleSaveMarkdown}
+        >
           Save Markdown
         </button>
       </div>
@@ -51,7 +86,12 @@ function Editor({ onMarkdownChange }) {
                 <div key={entry.id} className="mb-2">
                   <div className="relative border border-green-500 p-2">
                     <div className="whitespace-pre-line">{entry.content}</div>
-                    <button className="text-red-500 font-mono px-2 py-1 rounded bg-gray-700 absolute top-3 right-3" onClick={() => handleDeleteMarkdown(entry.id)}>Delete</button>
+                    <button
+                      className="text-red-500 font-mono px-2 py-1 rounded bg-gray-700 absolute top-3 right-3"
+                      onClick={() => handleDeleteMarkdown(entry.id)}
+                    >
+                      Delete
+                    </button>
                   </div>
                 </div>
               ))}
@@ -59,14 +99,16 @@ function Editor({ onMarkdownChange }) {
           )}
         </div>
       )}
-      <textarea
-        className={`flex-grow bg-black text-green-500 font-mono p-4 rounded-lg outline-none resize-none ${showTab ? 'hidden' : ''}`}
-        value={markdown}
-        onChange={(e) => {
-          setMarkdown(e.target.value);
-          onMarkdownChange(e.target.value);
+      <MdEditor
+        className={`flex-grow bg-black text-green-500 font-mono p-4 rounded-lg outline-none resize-none ${
+          showTab ? 'hidden' : ''
+        }`}
+        onChange={({ text }) => {
+          setMarkdown(text);
+          onMarkdownChange(text);
         }}
-        placeholder="Type your markdown here..."
+        value={markdown}
+        view={{ menu: true, md: true, html: false }}
       />
     </div>
   );
